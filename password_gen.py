97e0a759a647de868f16df006c1d265f6ff78156
Joshua Hancock <jhancock@mac-mini.lan>
2021-02-10 14:57:01 -0700

    removing uneccesary comments

--- password_gen.py
+++ password_gen.py
@@ -7,7 +7,6 @@ die = [1,2,3,4,5,6]
 d = dict()
 die_roll = secrets.choice(die)
 
-# print(die_roll)
 with open("dieware_list.txt") as f:
     for line in f:
         (key, val) = line.split()
@@ -24,20 +23,5 @@ def generate_die_words(num_words=5):
 actual_words = list()
 for x in generate_die_words():
     actual_words.append(d[x])
-# print(actual_words)
 
 print(" ".join(actual_words))
-# print(d)
-# print(generate_word_number())
-# print(generate_die_words())
-# print(next(generate_die_words()))
-# print(next(generate_die_words()))
-# print(next(generate_die_words()))
-# print(next(generate_die_words()))
-# print(next(generate_die_words()))
-
-# print(next(generate_die_words()))
-# print(next(generate_die_words()))
-# print(next(generate_die_words()))
-# print(next(generate_die_words()))
-# print(next(generate_die_words()))
